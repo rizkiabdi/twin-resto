8d2f01f5aaaa01a4fe2174e535dc498a7cd7d563
rizkiabdi <rizkiabdi227@gmail.com>
2023-11-27 14:25:28 +0800

    update deploy

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import {route} from './src/routes/menu.js'
+const PORT = 5000
+const app = express()
+
+app.use('/api/v1',route)
+
+app.use((err,req,res,next)=>{    
+    return res.status(500).send('Error')
+})
+
+
+app.listen(PORT)
