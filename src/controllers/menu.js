5108d5f89750c657b03bc66986109ef9a24a66d7
rizkiabdi <rizkiabdi227@gmail.com>
2023-11-29 07:34:24 +0800

    Mengubah response category menu

--- src/controllers/menu.js
+++ src/controllers/menu.js
@@ -14,7 +14,7 @@ export  async function getMenuByCategory(req,res,next){
         const categoryName = req.query.category
         if(err) return next(err)
         const [rows] =  await connection.query(`SELECT m.name,m.descriptions,m.price FROM menu as m INNER JOIN category_menu  c on m.category_id = c.id LEFT JOIN  WHERE c.name = '${categoryName}'`)
-        if(rows.length == 0) return res.status(400).json({message:'Product tidak ada'})
+        if(rows.length == 0) return res.status(400).json({message:'Menu tidak ada'})
         return res.status(200).json(rows)
     } catch (error) {
        return next(error)
--- src/routes/menu.js
+++ src/routes/menu.js
@@ -2,5 +2,5 @@ import express from 'express'
 import { getMenu,getMenuByCategory } from '../controllers/menu.js'
 export const route = express.Router()
 
-route.get('/menu',getMenu,getMenuByCategory)
+route.get('/menu',getMenuByCategory)
 
