c97e9ae14deeddef438dd38624c9cae94c2061b5
rizkiabdi <rizkiabdi227@gmail.com>
2023-11-29 08:38:52 +0800

    Perbaiki kesalahan Sql menu.js

--- src/controllers/menu.js
+++ src/controllers/menu.js
@@ -12,7 +12,6 @@ export async function getMenu(req,res,next){
 export  async function getMenuByCategory(req,res,next){
     try {
         const categoryName = req.query.category
-        if(err) return next(err)
         const [rows] =  await connection.query(`SELECT m.name,m.price FROM menu as m INNER JOIN category_menu as c on m.category_id = c.id WHERE c.name = '${categoryName}'`)
         if(rows.length == 0) return res.status(400).json({message:'Menu tidak ada'})
         return res.status(200).json(rows)
