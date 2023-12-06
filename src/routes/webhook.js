eea3ba2e7099854bc7d3d81a16c357cdfa86a7a5
rizkiabdi <rizkiabdi227@gmail.com>
2023-12-07 07:48:16 +0800

    update parserjson

--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { webHook } from '../controllers/webhook.js'
+import { parserJson } from '../middleware/body-parser.js'
 export const route = express.Router()
 
-route.post('/webhook',webHook)
+route.post('/webhook',parserJson,webHook)
