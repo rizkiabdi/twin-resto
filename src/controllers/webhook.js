329fabf51e4fbe71281ded07fdee858aa795f3a0
rizkiabdi <rizkiabdi227@gmail.com>
2023-12-07 08:45:30 +0800

    update status webhook

--- src/controllers/webhook.js
+++ src/controllers/webhook.js
@@ -2,9 +2,9 @@ import { connection } from "../config/db.js"
 export async function webHook(req,res){
     try {
         const {order_id,transaction_status} = req.body
-        if(transaction_status === "pending"){
+        if(transaction_status == "pending"){
             const tambah = await connection.query(`INSERT INTO payment VALUES('${transaction_status}','${order_id}'`)
-        }else if(transaction_status === "settlement" || transaction_status === "capture" ){
+        }else if(transaction_status == "settlement" || transaction_status == "capture" ){
             const update = await connection.query(`UPDATE payment SET status='${transaction_status}' WHERE order_id='${order_id}'`)
         }
         return res.status(200).json({status:ok})
