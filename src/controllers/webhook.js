009c700bd0e7a0429c6d7d4fb6364aabf96d37d7
rizkiabdi <rizkiabdi227@gmail.com>
2023-12-07 08:14:47 +0800

    update response webhook

--- src/controllers/webhook.js
+++ src/controllers/webhook.js
@@ -1,6 +1,11 @@
 import { connection } from "../config/db.js"
 export async function webHook(req,res){
-    const {transaction_status} = req.body
-    const rows = await connection.query(`INSERT INTO payment VALUES('${transaction_status}')`)
-    return res.status(200).json({status:ok})
+    try {
+        const {transaction_status} = req.body
+        const rows = await connection.query(`INSERT INTO payment VALUES('${transaction_status}')`)
+        return res.status(200).json({status:ok})
+        
+    } catch (error) {
+        return res.json(error)
+    }
 }
